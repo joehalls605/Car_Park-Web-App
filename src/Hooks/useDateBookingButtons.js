64b483d38b1c7fc0c7b746431382f5f9eca56c9a
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2023-10-17 09:27:59 +0100

    Button date colour completed

--- src/Hooks/useDateBookingButtons.js
+++ src/Hooks/useDateBookingButtons.js
@@ -7,54 +7,35 @@ const useDateBookingButtons = () => {
     dates.slice(rowIndex * 7, (rowIndex + 1) * 7)
   );
 
-  /* 
-  
-loop through the times, if the time is booked, update a variable number.
-Depending on the variable number, update the button colour to display different hue of colour ranging from green to amber to red. The variable
-  Below is just set up but hopefully on right path.
-
-*/
-
-  const datesTotal = () => {
-    
-    const bookingTotal = 0;
-    dates.forEach(date => {
-      if(date.timeSlots.booked === true){
-        bookingTotal ++;
+  const datesColour = (timeSlots) => {
+    let bookingTotal = 0;
+    timeSlots.forEach((slot) => {
+      if (slot.booked === true) {
+        bookingTotal++;
       }
     });
 
-    return datesTotal
-  } 
-
-  // const datesColoured = (datesTotal) => {
-  //   const dateColour = null;
-   
-  //   if(datesTotal <= 5){
-  //     dateColour = "bg-green-700"
-  //   }
-  //   if(datesTotal >= 5 && 9){
-  //     dateColour = "bg-amber-700"
-  //   }
-  //   dateColour = "bg-red-700"
-   
-  //   return dateColour
-  // } 
-
- //
+    if (bookingTotal > 9) {
+      return "bg-red-700 text-xl text-white px-4 py-2 rounded hover:bg-blue-800 transition-colors duration-300";
+    } else if (bookingTotal >= 5 && bookingTotal < 9) {
+      return "bg-orange-700 text-xl text-white px-4 py-2 rounded hover:bg-blue-800 transition-colors duration-300";
+    } else {
+      return "bg-green-700 text-xl text-white px-4 py-2 rounded hover:bg-blue-800 transition-colors duration-300";
+    }
+  };
 
   return (
     <div className="grid grid-cols-7 gap-2 p-10">
       {rows.map((row, rowIndex) => (
         <React.Fragment key={rowIndex}>
-          {row.map((date) => (
-            <button
-              key={date._id}
-              className="bg-blue-700 text-xl text-white px-4 py-2 rounded hover:bg-blue-800 transition-colors duration-300"
-            >
-              {date.text}
-            </button>
-          ))}
+          {row.map((date) => {
+            const buttonClass = datesColour(date.timeSlots);
+            return (
+              <button key={date._id} className={buttonClass}>
+                {date.text}
+              </button>
+            );
+          })}
         </React.Fragment>
       ))}
     </div>
